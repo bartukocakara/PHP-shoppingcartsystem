fa2d827ed363c9b43d6a1ff9c6e6a73796d59a40
bartukckr <bartu@labmob.com>
2020-04-23 19:00:28 +0300

    Route through placeorder page

--- cart.php
+++ cart.php
@@ -58,5 +58,12 @@
     //Prevent form submission
     header('location:index.php?page=cart');
     exit;
+
+    //Send the user to the place order page if they click the Place Order button, also the cart should not be empty
+    if(isset($_POST['placeorder']) && isset($_SESSION['cart']) && !empty($_SESSION['cart']))
+    {
+        header('Location:index.php?pae=placeholder');
+        exit;
+    }
     
 ?>
