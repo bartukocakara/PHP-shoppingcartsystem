99e024cace5f078fa79aac27cdd60c5f4c46f7ed
bartukckr <bartu@labmob.com>
2020-04-23 22:10:32 +0300

    Get products in Cart and Select from the Database

--- cart.php
+++ cart.php
@@ -65,5 +65,27 @@
         header('Location:index.php?pae=placeholder');
         exit;
     }
+
+    //Check the session variable for products in cart
+    $products_in_cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
+    $products = array();
+    $subtotal = 0.00;
     
+    //If there are product in cart
+    if($products_in_cart)
+    {
+        //There are products in the cart so we need to select thos products from the database
+        //Products in cart array to question martk string array, we need the SQL statement to include IN (?, ?, ?,...etc)
+        $array_to_question_marks = implode(',', array_fill(0, count($products_in_cart), '?'));
+        $stmt = $pdo->prepare('SELECT * FROM products WHERE id IN ('.$array_to_question_marks . ')');
+        //We only need the keys not the values, the keys are the id's of the products
+        $stmt->execute(array_key_exists($products_in_cart));
+        //Fetch the products from the database and return  the result as an Array
+        $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //Calculate the subtotal
+        foreach($products as $product)
+        {
+            $subtotal += (float)$product['price'] * (int)$products_in_cart[$product['id']];
+        }
+    }
 ?>
