b278acf90f9369ce624215062e0fe512946beca1
bartukckr <bartu@labmob.com>
2020-04-23 18:56:27 +0300

    update the cart function

--- cart.php
+++ cart.php
@@ -33,8 +33,30 @@
         header('location:index.php?page=cart');
         exit;
     }
+    // Remove product from cart, check for the URL param "remove", this is the product id, make sure it's a number and check if it's in the cart,
     if(isset($_GET['remove']) && is_numeric($_GET['remove']) && isset($_SESSION['remove']) && isset($_SESSION['cart'][$_GET['remove']])){
         //Remove the product from the sopping cart
         unset($_SESSION['cart'][$_GET['remove']]);
     }
+
+    //Update product quantities in cart if the user clicks the "Update" button on the shopping cart page
+    if(isset($_POST['update']) && isset($_SESSION['cart']))
+    {
+        // Loop through the post data so we can update the quantities for every product in cart
+        foreach($_POST as $k=>$v) {
+            if(strpos($k, 'quantity') !== false && is_numeric($v)) {
+                $id = str_replace('quantity-', '', $k);
+                $quantity = (int)$v;
+                //Always do checksand validation
+                if(is_numeric($id) && isset($_SESSION['cart'][$id]) && $quantity > 0) {
+                    //Update new quantity
+                    $_SESSION['cart'][$id] = $quantity;
+                } 
+            }
+        }
+    }
+    //Prevent form submission
+    header('location:index.php?page=cart');
+    exit;
+    
 ?>
